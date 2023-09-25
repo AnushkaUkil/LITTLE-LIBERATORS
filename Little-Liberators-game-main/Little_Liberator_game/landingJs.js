f7273c53b9732f81d126055102e457651a96264f
AnushkaUkil <117565874+AnushkaUkil@users.noreply.github.com>
2023-09-25 19:12:41 +0530

    Add files via upload

---
+++ Little-Liberators-game-main/Little_Liberator_game/landingJs.js
@@ -0,0 +1,7 @@
+// const loginBtn = document.getElementById("signIn-btn");
+let login = document.getElementById("login");
+
+// loginBtn.addEventListener("click",()=>{
+//     login.innerHTML = "logged";
+// });
+
